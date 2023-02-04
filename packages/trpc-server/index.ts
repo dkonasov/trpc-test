887d14ba04d593a8afb4f41498bffb817482b169
Konasov Dmitriy <d.konasov@yandex.ru>
2023-02-04 13:41:22 +0300

    added list route

--- packages/trpc-server/index.ts
+++ packages/trpc-server/index.ts
@@ -1,9 +1,32 @@
 import { initTRPC } from "@trpc/server";
-import { createHTTPServer } from '@trpc/server/adapters/standalone';
+import { createHTTPServer } from "@trpc/server/adapters/standalone";
+
+interface Entity {
+  id: number;
+  name: string;
+}
 
 const app = initTRPC.create();
 
-const router = app.router({});
+const router = app.router({
+  list: app.procedure.query(() => {
+    return new Promise<Entity[]>((resolve) => {
+      setTimeout(
+        () =>
+          resolve([
+            {
+              id: 0,
+              name: "foo",
+            },
+            {
+              id: 1,
+              name: "bar",
+            },
+          ]),
+        2000
+      );
+    });
+  }),
+});
 
-createHTTPServer({router}).listen(3000);
-
+createHTTPServer({ router }).listen(3000);
