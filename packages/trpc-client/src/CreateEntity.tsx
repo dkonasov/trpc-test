599d5ff431565e6e1b249706045e199751bbdb9c
dkonasov <d.konasov@yandex.ru>
2023-02-11 15:15:40 +0000

    connect entity creation form to backend

--- packages/trpc-client/src/CreateEntity.tsx
+++ packages/trpc-client/src/CreateEntity.tsx
@@ -1,10 +1,30 @@
-import { FC } from "react";
+import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import { ChangeEvent, FC, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AppRouter } from "trpc-server";
+
+const trpc = createTRPCProxyClient<AppRouter>({
+  links: [httpBatchLink({ url: import.meta.env.VITE_BACKEND_URL })],
+});
 
 export const CreateEntity: FC = () => {
+  const [ value, setValue ] = useState('');
+
+  const navigate = useNavigate();
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue((event.currentTarget as HTMLInputElement).value);
+  };
+
+  const save = async () => {
+    await trpc.create.mutate({ name: value});
+    navigate('/');
+  }
+
   return (
     <div>
-      <input type="text" />
-      <button type="button">Create</button>
+      <input type="text" onChange={onChange}/>
+      <button type="button" onClick={save}>Create</button>
     </div>
   );
 };
